38dbac06b1f3f125daadc593d5fd452a0c4781f1
MoorishQubit <zakaria.mzaouali@gmail.com>
2023-03-28 15:27:17 +0200

    code for random two qubit states in Julia

--- 2qubit.jl
+++ 2qubit.jl
@@ -1,27 +1,27 @@
 using QuantumInformation, Plots, LinearAlgebra, TensorOperations, HCubature, MultiQuad
 
 
-function state(r)
+function state(r) # randon 2 qubit state with fixed rank r
     h = rand(HaarKet{2}(4*r))
     ρ=ptrace(h,[4,r],2)
     return ρ
 end
 
-function wootters(r,x1,p1,x2,p2)
+function wootters(r,x1,p1,x2,p2) # discrete Wigner function for 2 qubits: Wootters construction
     kernel1=(1/2)*(Matrix(I,2,2)+((-1)^x1)*sz+((-1)^p1)*sx+((-1)^(x1+p1))*sy)
     kernel2=(1/2)*(Matrix(I,2,2)+((-1)^x2)*sz+((-1)^p2)*sx+((-1)^(x2+p2))*sy)
     w=(1/4)*tr((kernel1 ⊗ kernel2)*state(r))
     return real(w)
 end
 
-function tilma(r,t1,f1,t2,f2)
+function tilma(r,t1,f1,t2,f2) # Wigner function for arbitrary quantum systems: Tilma Construction
     kernel1=(1/2)*(Matrix(I,2,2)-sqrt(3)*(cos(2*t1)*sz+sin(2*t1)*(cos(2*f1)*sx+sin(2*f1)*sy)))
     kernel2=(1/2)*(Matrix(I,2,2)-sqrt(3)*(cos(2*t2)*sz+sin(2*t2)*(cos(2*f2)*sx+sin(2*f2)*sy)))
     w=tr((kernel1 ⊗ kernel2)*state(r))
     return real(w)
 end
 
-function negativity_wootters(r)
+function negativity_wootters(r) #Negativity of the Wigner function under Wootters construction
     s=[]
     for x1 in [0,1]
         for p1 in [0,1]
@@ -35,14 +35,15 @@ function negativity_wootters(r)
     return sum(s)
 end
 
+negativity_wootters(4)
 
-function negativity_tilma(r)
+function negativity_tilma(r) #Negativity of the Wigner function under Tilma construction
     #intg=dblquad((t,f) -> tilma(r,t,f,t,f)*(sin(2*t)/pi),0,pi/2,0,2*pi)[1]
     intg=hcubature(x-> tilma(r,x[1],x[2],x[1],x[2])*(sin(2*x[1])/pi),(0,0), (pi/2,2*pi))[1]
     return intg
  end
 
-negativity_wootters(4)
+
 
 
 # c=Any[]; neg_w=Any[]; neg_t=Any[]; neg=Any[]
