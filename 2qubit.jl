ae6e8642b8440899811d05cc5d56dd275eac26e2
MoorishQubit <zakaria.mzaouali@gmail.com>
2023-03-28 14:40:08 +0200

    code for random 2 qubit DM

--- 2qubit.jl
+++ 2qubit.jl
@@ -1,8 +1,57 @@
-using QuantumInformation, Plots, LinearAlgebra, TensorOperations
+using QuantumInformation, Plots, LinearAlgebra, TensorOperations, HCubature, MultiQuad
+
 
 function state(r)
-    ρ=rand(HilbertSchmidtStates{2, r}(4))
+    h = rand(HaarKet{2}(4*r))
+    ρ=ptrace(h,[4,r],2)
+    return ρ
+end
+
+function wootters(r,x1,p1,x2,p2)
+    kernel1=(1/2)*(Matrix(I,2,2)+((-1)^x1)*sz+((-1)^p1)*sx+((-1)^(x1+p1))*sy)
+    kernel2=(1/2)*(Matrix(I,2,2)+((-1)^x2)*sz+((-1)^p2)*sx+((-1)^(x2+p2))*sy)
+    w=(1/4)*tr((kernel1 ⊗ kernel2)*state(r))
+    return real(w)
+end
+
+function tilma(r,t1,f1,t2,f2)
+    kernel1=(1/2)*(Matrix(I,2,2)-sqrt(3)*(cos(2*t1)*sz+sin(2*t1)*(cos(2*f1)*sx+sin(2*f1)*sy)))
+    kernel2=(1/2)*(Matrix(I,2,2)-sqrt(3)*(cos(2*t2)*sz+sin(2*t2)*(cos(2*f2)*sx+sin(2*f2)*sy)))
+    w=tr((kernel1 ⊗ kernel2)*state(r))
+    return real(w)
+end
+
+function negativity_wootters(r)
+    s=[]
+    for x1 in [0,1]
+        for p1 in [0,1]
+            for x2 in [0,1]
+                for p2 in [0,1]
+                    push!(s,wootters(r,x1,p1,x2,p2))
+                end
+            end
+        end
+    end
+    return sum(s)
 end
 
 
-state(1)
+function negativity_tilma(r)
+    #intg=dblquad((t,f) -> tilma(r,t,f,t,f)*(sin(2*t)/pi),0,pi/2,0,2*pi)[1]
+    intg=hcubature(x-> tilma(r,x[1],x[2],x[1],x[2])*(sin(2*x[1])/pi),(0,0), (pi/2,2*pi))[1]
+    return intg
+ end
+
+negativity_wootters(4)
+
+
+# c=Any[]; neg_w=Any[]; neg_t=Any[]; neg=Any[]
+# for x in 1:10000
+#     push!(c,concurrence(state(1)))
+#     push!(neg,negativity(state(1),[2,2],2))
+# end
+
+# scatter(neg, c, label="rank 1")
+# ylabel!("Concurrence")
+# xlabel!("Negativity Entanglement")
+
--- python.py
+++ python.py
@@ -6,7 +6,7 @@ from toqito.state_props import negativity
 from scipy import integrate as integrate
 import itertools
 
-plt.style.use('seaborn-white')
+plt.style.use('seaborn-v0_8-white')
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = ['Times New Roman']
 plt.rcParams['font.weight'] = 'normal'
@@ -15,35 +15,15 @@ plt.rcParams['mathtext.fontset'] = 'stix'
 marker = itertools.cycle(('o', '+', 'x','*')) 
 
 
-# def state(r):
-#     s=ket2dm(rand_ket(4*r))
-#     s.dims=[[4,r],[4,r]]
-#     ss=s.ptrace(0)
-#     ss.dims=[[2,2],[2,2]]
-#     return ss
-
 def state(r):
-    if r==1:
-        rho=Qobj(np.matrix([[0.861553+0.0*1j,-0.189292+0.00294553*1j,0.135905-0.0936245*1j,0.173844+0.161189*1j],
-                            [-0.189292-0.00294553*1j,0.0415995+0.0*1j,-0.0301798+0.0201056*1j,-0.0376444-0.0360094*1j],
-                            [0.135905+0.0936245*1j,-0.0301798-0.0201056*1j,0.0316123+0.0*1j,0.00990655+0.0443182*1j],
-                            [0.173844-0.161189*1j,-0.0376444+0.0360094*1j,0.00990655-0.0443182*1j,0.0652356+0.0*1j]]),dims=[[2,2],[2,2]])
-    elif r==2:
-        rho=Qobj(np.matrix([[0.076281+0.0*1j,-0.068415-0.146578*1j,0.0103318+0.0120563*1j,-0.0774808+0.00323882*1j],
-                            [-0.068415+0.146578*1j,0.609089+0.0*1j,0.0562163-0.0605154*1j,-0.15758-0.1321*1j],
-                            [0.0103318-0.0120563*1j,0.0562163+0.0605154*1j,0.0510243+0.0*1j,-0.0887119-0.038489*1j],
-                            [-0.0774808-0.00323882*1j,-0.15758+0.1321*1j,-0.0887119+0.038489*1j,0.263606+0.0*1j]]),dims=[[2,2],[2,2]])
-    elif r==3:
-        rho=Qobj(np.matrix([[0.452554+0.0*1j,-0.137758+0.0173835*1j,0.0909171-0.110103*1j,0.0553365-0.16667*1j],
-                            [-0.137758-0.0173835*1j,0.0894731+0.0*1j,-0.0505617+0.0331408*1j,-0.00604473+0.13467*1j],
-                            [0.0909171+0.110103*1j,-0.0505617-0.0331408*1j,0.077898+0.0*1j,0.0434916+0.00517076*1j],
-                            [0.0553365+0.16667*1j,-0.00604473-0.13467*1j,0.0434916-0.00517076*1j,0.380075+0.0*1j]]),dims=[[2,2],[2,2]])
-    elif r==4:
-        rho=Qobj(np.matrix([[0.139614+0.0*1j,-0.059128+0.0722803*1j,-0.0216255-0.010379*1j,-0.0380448-0.121063*1j],
-                            [-0.059128-0.0722803*1j,0.142448+0.0*1j,-0.068477+0.155433*1j,-0.0532428+0.122709*1j],
-                            [-0.0216255+0.010379*1j,-0.068477-0.155433*1j,0.388016+0.0*1j,0.096868+0.0504258*1j],
-                            [-0.0380448+0.121063*1j,-0.0532428-0.122709*1j,0.096868-0.0504258*1j,0.329921+0.0*1j]]),dims=[[2,2],[2,2]])
-    return rho
+    s=ket2dm(rand_ket_haar(4*r))
+    s.dims=[[4,r],[4,r]]
+    ss=s.ptrace(0)
+    ss.dims=[[2,2],[2,2]]
+    return ss
+
+# def state(r):
+#     return rand_dm_ginibre(4, rank=r,dims=[[2,2], [2,2]])
 
 def wootters(r,x1,p1,x2,p2):
     kernel1=(1.0/2.0)*(qeye(2)+((-1.)**x1) * sigmaz()+((-1.)**p1) * sigmax()+((-1.)**(x1+p1)) * sigmay())
@@ -63,14 +43,14 @@ def negativity_wootters(r):
         for p1 in [0,1]:
             for x2 in [0,1]:
                 for p2 in [0,1]:
-                    summ+=[np.abs(wootters(r,x1,p1,x2,p2))]
-    return sum(summ) - 1.0
+                    summ+=[wootters(r,x1,p1,x2,p2)]
+    return sum(summ) 
 
-def negativity_tilma(r):
-    #options={'limit':5}
-    return integrate.nquad(lambda t,f: tilma(r,t,f,t,f)*np.sin(2*t)/np.pi,[[0,np.pi/2],[0,2*np.pi]])#,opts=[options,options])[0]
+# def negativity_tilma(r):
+#     #options={'limit':5}
+#     return integrate.nquad(lambda t,f: tilma(r,t,f,t,f)*np.sin(2*t)/np.pi,[[0,np.pi/2],[0,2*np.pi]])#,opts=[options,options])[0]
 
-print(negativity_tilma(4))
+print(state(1))
 
 # for x in np.linspace(0,1,100):
 #     print(matrix_rank(rand_dm(N=4,density=x)))
