637472ef42daf03d2d2481021312a11bf39435f8
zakaria <zakaria.mzaouali@gmail.com>
2023-03-29 08:47:00 +0200

    seaborn style

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import pylab as pl
+pl.style.use('seaborn-v0_8-white')
 pl.rcParams.update({
     "text.usetex": True,
 })
@@ -8,10 +9,15 @@ for r in [1, 2, 3, 4]:
     ax[0].scatter(d[:, 0], d[:, 1], s=1)
     ax[1].scatter(d[:, 0], d[:, 2], s=1)
 
-    ax[0].set_xlabel("Wigner negativity")
+    ax[0].set_xlabel("Wootters negativity")
     ax[0].set_ylabel("Negativity")
 
-    ax[1].set_xlabel("Wigner negativity")
+    ax[1].set_xlabel("Wootters negativity")
     ax[1].set_ylabel("Log negativity")
+
+    #ax[0].tick_params(axis='both', which='major', labelsize=12)
+    #ax[1].tick_params(axis='both', which='major', labelsize=12)
+
+    fig.tight_layout()
     fig.savefig(f"figs/plot_{r}.png")
     pl.close()
